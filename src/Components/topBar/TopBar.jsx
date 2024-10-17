7c166dd6d95bf685460041de70bf1e54149a620a
Michael Hosamani <michaelhosamani26@gmail.com>
2024-10-17 23:07:50 +0530

    implemented dropdown menu successfully

--- src/Components/topBar/TopBar.jsx
+++ src/Components/topBar/TopBar.jsx
@@ -12,67 +12,111 @@ function TopBar(){
                 </h1>
 
                 <div className="flex gap-6">
+
+                    
                     <div className="hover:cursor-pointer relative group">
+                        <Link to="/exam" className="relative z-10">
+                            <div className='border-box py-7'> Exams </div>
+                        </Link>
 
-                            <Link to="/exam">
-                                Exams
-                            </Link>
+                        
+                        <span className="hidden bg-blue-300 w-[3.2rem] h-[0.25rem] rounded-full absolute top-14 left-[-2px] group-hover:inline-block"></span>
 
-                        <span className='hidden bg-blue-300  w-[3.2rem] h-[0.25rem] rounded-full absolute top-7 left-[-2px] group-hover:inline-block'></span>
-                        <div className='hidden bg-[#245190] w-[14rem] h-36 absolute top-[3.2rem] left-[-0.6rem] group-hover:inline-block rounded-2xl' >
-                            <div className='flex flex-col items-start p-4 gap-4'>
-                                <div>Neet</div>
-                                <div>Jee</div>
-                                <div>Class 6-10</div>
-                            </div>
+                        <div className="hidden group-hover:flex  absolute top-[4.9rem] left-[-0.6rem] w-[14rem] h-36 bg-[#245190] rounded-2xl flex-col items-start p-4 gap-4 z-20">
+                        <Link to="/exam" className="text-white hover:text-blue-300 ">
+                            <p className='hover:cursor-pointer'>Neet</p> 
+                        </Link>
+                        <Link to="/exam" className="text-white hover:text-blue-300">
+                            Jee
+                        </Link>
+                        <Link to="/exam" className="text-white hover:text-blue-300">
+                            Class 6-10
+                        </Link>
                         </div>
                     </div>
+
+
                     <div className="hover:cursor-pointer relative group">
                         <Link to="/programs">
-                            Programs
+                           <div className='border-box py-7'> Programs </div>
                         </Link>
                        
-                        <span className='hidden bg-blue-300  w-[4.6rem] h-[0.25rem] rounded-full absolute top-7 left-[-2px] group-hover:inline-block'></span>
-                        <div className='hidden bg-[#245190] w-[14rem] h-36 absolute top-[3.2rem] left-[-0.6rem] group-hover:inline-block rounded-2xl' >
+                        <span className='hidden bg-blue-300  w-[4.6rem] h-[0.25rem] rounded-full absolute top-14 left-[-2px] group-hover:inline-block'></span>
+                        <div className='hidden bg-[#245190] w-[14rem] h-36 absolute top-[4.9rem] left-[-0.6rem] group-hover:inline-block rounded-2xl' >
                             <div className='flex flex-col items-start p-4 gap-4'>
-                                <div>Online Programs</div>
-                                <div>Classroom Programs</div>
-                                <div>Distance Learning</div>
+                            <Link to="/programs" className="text-white hover:text-blue-300">
+                                Online Programs
+                            </Link>
+                            <Link to="/programs" className="text-white hover:text-blue-300">
+                                Classroom Programs
+                            </Link>
+                            <Link to="/programs" className="text-white hover:text-blue-300">
+                                Distance Learning
+                            </Link>
                             </div>
                         </div>
                     </div>
                     <div className="hover:cursor-pointer relative group">
-                        <Link to="/scholarships">Scholarships</Link>
-                        <span className='hidden bg-blue-300  w-[6rem] h-[0.25rem] rounded-full absolute top-7 left-[-2px] group-hover:inline-block'></span>
-                        <div className='hidden bg-[#245190] w-[14rem] h-26 absolute top-[3.2rem] left-[-0.6rem] group-hover:inline-block rounded-2xl' >
+                        <Link to="/scholarships">
+                            <div className='border-box py-7'> Scholarships </div>
+                        </Link>
+                        <span className='hidden bg-blue-300  w-[6rem] h-[0.25rem] rounded-full absolute top-14 left-[-2px] group-hover:inline-block'></span>
+                        <div className='hidden bg-[#245190] w-[14rem] h-26 absolute top-[4.9rem] left-[-0.6rem] group-hover:inline-block rounded-2xl' >
                             <div className='flex flex-col items-start p-4 gap-4'>
-                                <div>ADSAT</div>
-                                <div>TALENTEX</div>
+                            <Link to="/scholarships" className="text-white hover:text-blue-300">
+                                OADSAT
+                            </Link>
+                            <Link to="/scholarships" className="text-white hover:text-blue-300">
+                                TALENTEX
+                            </Link>
                             </div>
                         </div>
                     </div>
                     <div className="hover:cursor-pointer relative group">
-                        <Link to="test-series">Test Series</Link>
-                        <span className='hidden bg-blue-300  w-[5.3rem] h-[0.25rem] rounded-full absolute top-7 left-[-2px] group-hover:inline-block'></span>
-                        <div className='hidden bg-[#245190] w-[14rem] h-36 absolute top-[3.2rem] left-[-0.6rem] group-hover:inline-block rounded-2xl' >
+                        <Link to="/test-series">
+                            <div className='border-box py-7'>Test Series </div>
+                        </Link>
+                        <span className='hidden bg-blue-300  w-[5.3rem] h-[0.25rem] rounded-full absolute top-14 left-[-2px] group-hover:inline-block'></span>
+                        <div className='hidden bg-[#245190] w-[14rem] h-36 absolute top-[4.9rem] left-[-0.6rem] group-hover:inline-block rounded-2xl' >
                             <div className='flex flex-col items-start p-4 gap-4'>
-                                <div>Neet</div>
-                                <div>Jee(Main + Advance)</div>
-                                <div>Jee Main</div>
+                            <Link to="/test-series" className="text-white hover:text-blue-300">
+                                Neet
+                            </Link>
+                            <Link to="/test-series" className="text-white hover:text-blue-300">
+                                Jee(Main + Advance)
+                            </Link>
+                            <Link to="/test-series" className="text-white hover:text-blue-300">
+                                Jee Main
+                            </Link>
                             </div>
                         </div>
                     </div>
                     <div className="hover:cursor-pointer relative group">
-                        <Link to="study-materials">Study Materials</Link>
-                        <span className='hidden bg-blue-300 w-[7.4rem] h-[0.25rem] rounded-full absolute top-7 left-[-2px] group-hover:inline-block'></span>
-                        <div className='hidden bg-[#245190] w-[14rem] h-64 absolute top-[3.2rem] left-[-0.6rem] group-hover:inline-block rounded-2xl' >
+                        <Link to="study-materials">
+                            <div className='border-box py-7'> Study Materials </div>
+                        </Link>
+                        <span className='hidden bg-blue-300 w-[7.4rem] h-[0.25rem] rounded-full absolute top-14 left-[-2px] group-hover:inline-block'></span>
+                        <div className='hidden bg-[#245190] w-[14rem] h-64 absolute top-[4.9rem] left-[-0.6rem] group-hover:inline-block rounded-2xl' >
                             <div className='flex flex-col items-start p-4 gap-4'>
-                                <div>Jee Main</div>
-                                <div>Jee Advance</div>
-                                <div>Neet</div>
-                                <div>Ncert Solution</div>
-                                <div>CBSE</div>
-                                <div>Olympiad</div>
+                                <Link to="/study-materials" className="text-white hover:text-blue-300">
+                                    Jee Main
+                                </Link>
+                                <Link to="/study-materials" className="text-white hover:text-blue-300">
+                                    Jee Advance
+                                </Link>
+                                <Link to="/study-materials" className="text-white hover:text-blue-300">
+                                    Neet
+                                </Link>
+                                <Link to="/study-materials" className="text-white hover:text-blue-300">
+                                    Ncert Solution
+                                </Link>
+                                <Link to="/study-materials" className="text-white hover:text-blue-300">
+                                    CBSE
+                                </Link>
+                                <Link to="/study-materials" className="text-white hover:text-blue-300">
+                                    Olympiad
+                                </Link>
+                                
                             </div>
                         </div>
                     </div>
