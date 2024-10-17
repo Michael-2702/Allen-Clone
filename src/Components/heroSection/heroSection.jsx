546db1856510b513d2137d102c81a5831ed13b42
Michael Hosamani <michaelhosamani26@gmail.com>
2024-10-17 23:31:20 +0530

    done with left half of hero section

--- src/Components/heroSection/heroSection.jsx
+++ src/Components/heroSection/heroSection.jsx
@@ -2,8 +2,29 @@
 
 function HeroSection(){
 
-    return <div className="absolute top-32 text-black bg-blue-500">
-        HeroSection
+    return <div className="absolute w-full h-full top-20 text-white bg-[#0f1825]">
+       <div className="w-[940px] h-[450px]  text-white mx-auto my-0 flex">
+            <div className="w-[470px] flex flex-col justify-evenly">
+                <h3 className="text-4xl font-bold">
+                    Your Dream. <br></br>
+                    Our Expertise.
+                </h3>
+                <div >
+                    <p>
+                        What brings you to us today?
+                    </p>
+                    <div className="mt-5">
+                        <button className="border-2 border-blue-300 px-4 py-2 rounded-3xl mr-3 text-base">NEET</button>
+                        <button className="border-2 border-blue-300 px-4 py-2 rounded-3xl mr-3 text-base">JEE</button>
+                        <button className="border-2 border-blue-300 px-4 py-2 rounded-3xl text-base">Grade 6-10</button>
+                    </div>
+                </div>
+               
+            </div>
+            <div className="w-[470px]">
+
+            </div>
+       </div>
     </div>
 }
 
